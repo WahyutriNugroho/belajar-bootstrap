243d9fd9fc9b0874a17b8ceaf3df833236cd72ba
WahyutriNugroho <wahyutrinugroho594@gmail.com>
2021-02-02 13:35:35 +0700

    mengubah style dan merubah letak form pada kontak menjadi ketengah

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
 $('.page-scroll').on('click', function(e) {
 
-    var tujuan = $(this).attr('href');
-   
-    var elemenTujuan = $(tujuan);
-   
-    $('html , body').animate({
-     scrollTop: elemenTujuan.offset().top - 50
-    });
-   
-    e.preventDefault();
+    console.log('ok');
    });
+
+
+  
