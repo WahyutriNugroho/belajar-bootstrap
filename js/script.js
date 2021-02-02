110d5e132e188816192b86faeef44b746247fd8c
WahyutriNugroho <wahyutrinugroho594@gmail.com>
2021-02-02 14:39:34 +0700

    mengubah file dan mengahpus beberapa

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 $('.page-scroll').on('click', function(e) {
 
-    console.log('ok');
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top - 50
+    });
+   
+    e.preventDefault();
    });
 
 
-  
+//   menyimpan parallx
+
+//jumbotron
+$(window).scroll(function(){
+    let wScroll =$(this).scrollTop();
+    // console.log(wScroll)
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px,'+ wScroll/4+'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px,'+ wScroll/2+'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px,'+ wScroll/1.2+'%)'
+    });
+
+
+
+});
